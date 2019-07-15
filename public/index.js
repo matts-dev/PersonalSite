1728e974c0c233a5dc880d73832cdc5c88092498
dev-user <no.email>
2019-07-14 21:21:10 -0400

    working on basic home page.

---
+++ public/index.js
@@ -0,0 +1,73 @@
+const px_to_em = 16;
+const em_to_px = 1 / px_to_em;
+
+
+var collapseMap = {};
+var openItem = null;
+function handleCollapsibleClicked(element, targetContentId)
+{
+    targetContent = document.getElementById(targetContentId)
+
+
+    let collapseItem = collapseMap[element.id];
+    if(collapseItem == null)
+    {
+        collapseItem = {
+            id : element.id,
+            button : element,
+            open : false,
+            content : targetContent
+        }
+        collapseMap[element.id] = collapseItem;
+    }
+
+
+    if(!collapseItem.open)
+    {
+        collapseItem.open = true;
+        collapseItem.content = targetContent;
+
+        let targetHeight = targetContent.scrollHeight;
+        targetContent.style.maxHeight = targetHeight + "px";
+
+        //collapse another item if we've already opened it.
+        if(openItem != null)        
+        {
+            openItem.open = false;
+            openItem.content.style.maxHeight = 0;
+        }
+        openItem = collapseItem;
+    }
+    else{
+        collapseItem.open = false;
+        targetContent.style.maxHeight = 0;
+        openItem = null;
+        
+    }
+}
+
+var sidebarOpen = false;
+function openSidebar()
+{
+    // document.getElementById("sidebar").style.width = "2em";
+    document.getElementById("sidebar").style.width = "50%";
+
+}
+
+function closeSidebar()
+{
+    document.getElementById("sidebar").style.width = "0";
+}
+
+loadContent("tests/embedtest.html");
+function loadContent(content){
+    var contentElement = document.getElementById("content");
+    if(contentElement)
+    {
+        contentStr = '<object type="text/html" data=' + content + '></object>'
+        contentElement.innerHTML = contentStr;
+    }
+    else{
+        Console.log("No content for this page")
+    }
+}
