ed90a6295d83b92d86acf0714316a37449f9432e
dev-w <no@email.com>
2026-08-15 09:16:25 -0400

    fixing vector math demo to not include the wrong title, and removed the fullscreen button since it seemed broken.

--- public/pages/tuts/webgl1/vec_math_review/MollerTrumboreRayTriangle.js
+++ public/pages/tuts/webgl1/vec_math_review/MollerTrumboreRayTriangle.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\dev-w\AppData\Local\Temp\tmp1jnp9mcl.js
+// include: C:\Users\dev-w\AppData\Local\Temp\tmpm1oriny_.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -206,21 +206,21 @@ Module['FS_createPath']("/PreloadAssets/TutorialEngine", "font", true, true);
 
   })();
 
-// end include: C:\Users\dev-w\AppData\Local\Temp\tmp1jnp9mcl.js
-// include: C:\Users\dev-w\AppData\Local\Temp\tmpakl9j568.js
+// end include: C:\Users\dev-w\AppData\Local\Temp\tmpm1oriny_.js
+// include: C:\Users\dev-w\AppData\Local\Temp\tmperch0erb.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\dev-w\AppData\Local\Temp\tmpakl9j568.js
-// include: C:\Users\dev-w\AppData\Local\Temp\tmp2qoqqzyb.js
+  // end include: C:\Users\dev-w\AppData\Local\Temp\tmperch0erb.js
+// include: C:\Users\dev-w\AppData\Local\Temp\tmpgsin6ksd.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\dev-w\AppData\Local\Temp\tmp2qoqqzyb.js
+  // end include: C:\Users\dev-w\AppData\Local\Temp\tmpgsin6ksd.js
 
 
 var programArgs = [];
